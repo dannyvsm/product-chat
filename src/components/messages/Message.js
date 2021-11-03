81e74b6ad3cb8ed97dcad9f948557070aedfae0c
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-11-03 10:42:33 +0000

    delete unnecessary files

--- src/components/messages/Message.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-const Message = ({ messages }) => {
-  return messages.map((message, index) => (
-    <div key={message.id}>{message.text}</div>
-  ));
-};
-
-export default Message;
